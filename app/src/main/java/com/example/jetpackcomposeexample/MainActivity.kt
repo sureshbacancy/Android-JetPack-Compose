dbb42de35323510deb869502a8111653044c9d5e
bacancy <suresh.prajapati@bacancy.com>
2024-02-12 16:58:14 +0530

    Android JatPack Compose Demo ---------------------------
    - How to define Column and views inside column
    
    Column {
    
    	- Horizontal Alignment
    	- Vertical Arrangment
    }
    
    Row {
    
    	- Horizontal Arrangment
    	- Vertical Alignment
    }
    
    - Alignment.End, Alignment.Start, Alignment.CenterHorizontally
    - Arrangement.Top, Arrangement.Bottom, Arrangement.Center, Arrangement.SpaceAround, Arrangement.SpaceBetween, Arrangement.SpaceEvenly
    
    Date : 12-02-2024
    
    By Suresh

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,16 +3,43 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greetings("Android")
+           Column(
+               modifier = Modifier
+                   .fillMaxSize()         // Full height / width
+                  // .fillMaxSize(0.5f)     // Specific height / width
+                  // .height(200.dp)        // Specific height
+                  // .width(200.dp)         // Specific width
+                   .background(Color.Green),// Background Color
+               horizontalAlignment = Alignment.CenterHorizontally, // Horizontal Alignment
+               //Alignment.End, Alignment.Start, Alignment.CenterHorizontally
+               verticalArrangement = Arrangement.SpaceEvenly // Vertical Arrangement
+                // Arrangement.Top, Arrangement.Bottom, Arrangement.Center,
+                // Arrangement.SpaceAround, Arrangement.SpaceBetween, Arrangement.SpaceEvenly
+           ) {
+
+               Text(text = "Hello")
+               Text(text = "Android", color = Color.Red)
+               Text(text = "Kotlin")
+           }
         }
     }
 
