69399221e26efdbc5f339c034e848b78fcac6862
bacancy <suresh.prajapati@bacancy.com>
2024-06-26 15:28:33 +0530

    Date : 26-06-2024
    - Learnt use of Row and Columns
    - Make white and black cube design

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -4,41 +4,51 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-
+            ui()
             // Demo 10.2 : 14-02-2024
 
-      /*      LazyColumn {
+            /*  LazyColumn {
 
-                items(500000) {
-                    Text(
-                        text = "item $it",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 20.dp),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }*/
+                  items(100) {
+                      Text(
+                          text = "Item - ${it.plus(1)}",
+                          modifier = Modifier
+                              .fillMaxWidth()
+                              .padding(vertical = 20.dp),
+                          fontSize = 20.sp,
+                          fontWeight = FontWeight.Bold,
+                          textAlign = TextAlign.Center
+                      )
+                  }
+              }*/
 
             // Demo 10 : 14-02-2024
 
@@ -209,59 +219,59 @@ class MainActivity : ComponentActivity() {
 
             // Demo 8 : 13-02-2024
 
-            /* val snackBarHostState = remember { SnackbarHostState() }
-             var textFieldState by remember {
-                 mutableStateOf("")
-             }
-             val scope = rememberCoroutineScope()
+            /*   val snackBarHostState = remember { SnackbarHostState() }
+               var textFieldState by remember {
+                   mutableStateOf("")
+               }
+               val scope = rememberCoroutineScope()
 
 
-             Scaffold(
-                 modifier = Modifier.fillMaxSize(),
-                 snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
+               Scaffold(
+                   modifier = Modifier.fillMaxSize(),
+                   snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
 
-             ) {
+               ) {
 
-                 Column(
-                     horizontalAlignment = Alignment.CenterHorizontally,
-                     verticalArrangement = Arrangement.Center,
-                     modifier = Modifier
-                         .fillMaxSize()
-                         .padding(horizontal = 30.dp)
-                         .background(Color.White)
-                 ) {
+                   Column(
+                       horizontalAlignment = Alignment.CenterHorizontally,
+                       verticalArrangement = Arrangement.Center,
+                       modifier = Modifier
+                           .fillMaxSize()
+                           .padding(horizontal = 30.dp)
+                           .background(Color.White)
+                   ) {
 
-                     TextField(
-                         value = textFieldState, onValueChange = {
-                             textFieldState = it
-                         },
-                         label = {
-                             Text(text = "Enter Your Name")
-                         },
-                         singleLine = true,
-                         modifier = Modifier.fillMaxWidth(),
-                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done)
+                       TextField(
+                           value = textFieldState, onValueChange = {
+                               textFieldState = it
+                           },
+                           label = {
+                               Text(text = "Enter Your Name")
+                           },
+                           singleLine = true,
+                           modifier = Modifier.fillMaxWidth(),
+                           keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done)
 
-                     )
-                     Spacer(modifier = Modifier.height(16.dp))
-                     Button(onClick = {
+                       )
+                       Spacer(modifier = Modifier.height(16.dp))
+                       Button(onClick = {
 
-                         if (textFieldState.isEmpty()){
-                             scope.launch {
-                                 snackBarHostState.showSnackbar("Please enter name")
-                             }
-                         }else {
-                             scope.launch {
-                                 snackBarHostState.showSnackbar(textFieldState)
-                             }
-                         }
+                           if (textFieldState.isEmpty()){
+                               scope.launch {
+                                   snackBarHostState.showSnackbar("Please enter name")
+                               }
+                           }else {
+                               scope.launch {
+                                   snackBarHostState.showSnackbar(textFieldState)
+                               }
+                           }
 
 
-                     }) {
-                         Text(text = "Submit")
-                     }
-                 }
-             }*/
+                       }) {
+                           Text(text = "Submit")
+                       }
+                   }
+               }*/
 
 
             // Demo 7 : 13-02-2024
@@ -281,63 +291,62 @@ class MainActivity : ComponentActivity() {
 
             // Demo 6 : 13-02-2024
 
-            /* val fontFamily = FontFamily(
-                 Font(R.font.protest_revolution_regular, FontWeight.Thin),
-                 Font(R.font.titillium_web_black, FontWeight.Thin),
-             )
+            /*   val fontFamily = FontFamily(
+                   Font(R.font.protest_revolution_regular, FontWeight.Thin),
+                   Font(R.font.titillium_web_black, FontWeight.Thin),
+               )
 
-             Box(
-                 modifier = Modifier
-                     .fillMaxSize()
-                     .background(Color.White)
-                     .padding(15.dp),
-                 contentAlignment = Alignment.Center
+               Box(
+                   modifier = Modifier
+                       .fillMaxSize()
+                       .background(Color.White)
+                       .padding(15.dp),
+                   contentAlignment = Alignment.Center
 
 
-             ) {
-                 Text(
-                     text = buildAnnotatedString {
-                         withStyle(
-                             style = SpanStyle(
-                                 color = Color.Red,
-                                 fontSize = 60.sp
-                             )
-                         ) {
-                             append("A")
-                         }
-                         append("ndroid   ")
+               ) {
+                   Text(
+                       text = buildAnnotatedString {
+                           withStyle(
+                               style = SpanStyle(
+                                   color = Color.Black,
+                                   fontSize = 60.sp
+                               )
+                           ) {
+                               append("A")
+                           }
+                           append("ndroid   ")
 
-                         withStyle(
-                             style = SpanStyle(
-                                 color = Color.Red,
-                                 fontSize = 60.sp
-                             )
-                         ) {
-                             append("J")
-                         }
-                         append("etpack")
-                     },
-                     color = Color.Black,
-                     fontSize = 28.sp,
-                     fontFamily = fontFamily,
-                     fontWeight = FontWeight.ExtraBold,
-                     textAlign = TextAlign.Center
-                 )
-             }*/
+                           withStyle(
+                               style = SpanStyle(
+                                   color = Color.Black,
+                                   fontSize = 60.sp
+                               )
+                           ) {
+                               append("J")
+                           }
+                           append("etpack")
+                       },
+                       color = Color.Black,
+                       fontSize = 28.sp,
+                       fontFamily = fontFamily,
+                       fontWeight = FontWeight.ExtraBold,
+                       textAlign = TextAlign.Center
+                   )
+               }*/
 
 
             // Demo 5 : 13-02-2024
 
-            /*       val painter = painterResource(id = R.drawable.bg_image)
-                   val description = "Card Image demo"
-                   val title = "Card Image Demo"
-                   Box(
-                       modifier = Modifier
-                           .padding(16.dp)
-                   ) {
-                       ImageCard(painter = painter, title = title, contentDescription = description)
-                   }
-       */
+            /* val painter = painterResource(id = R.drawable.bg_image)
+             val description = "Card Image demo"
+             val title = "Card Image Demo"
+             Box(
+                 modifier = Modifier
+                     .padding(16.dp)
+             ) {
+                 ImageCard(painter = painter, title = title, contentDescription = description)
+             }*/
 
             // Demo 4 : 13-02-2024
 
@@ -352,7 +361,7 @@ class MainActivity : ComponentActivity() {
                  verticalArrangement = Arrangement.Center
              ) {
                  Text(
-                     text = "Hello Bacancy",
+                     text = "Hello Android",
                      color = Color.Black,
                      fontSize = 40.sp,
                      modifier = Modifier
@@ -364,26 +373,26 @@ class MainActivity : ComponentActivity() {
 
             // Demo 3 : 13-02-2024
 
-            /*  Column(
-                  modifier = Modifier
-                      .padding(20.dp)
-                      .fillMaxSize()
-                      // .fillMaxWidth() // something like Match_Parent
-                      // .fillMaxHeight()
-                      .background(Color.Cyan),
-                  horizontalAlignment = Alignment.CenterHorizontally,
-                  verticalArrangement = Arrangement.Center
-              ) {
-                  Text(
-                      text = "Hello Dell",
-                      color = Color.White,
-                      fontSize = 50.sp,
-                      modifier = Modifier
-                          .background(Color.Red)
-                          .padding(20.dp)
+            /*   Column(
+                   modifier = Modifier
+                       .padding(20.dp)
+                       .fillMaxSize()
+                       // .fillMaxWidth() // something like Match_Parent
+                       // .fillMaxHeight()
+                       .background(Color.Cyan),
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Center
+               ) {
+                   Text(
+                       text = "Hello Dell",
+                       color = Color.White,
+                       fontSize = 50.sp,
+                       modifier = Modifier
+                           .background(Color.Black)
+                           .padding(20.dp)
 
-                  )
-              }*/
+                   )
+               }*/
 
 
             // Demo 2 : 12-02-2024
@@ -394,7 +403,7 @@ class MainActivity : ComponentActivity() {
                       // .fillMaxSize(0.5f)     // Specific height / width
                       // .height(200.dp)        // Specific height
                       // .width(200.dp)         // Specific width
-                      .background(Color.Red),// Background Color
+                      .background(Color.Black),// Background Color
 
                   horizontalArrangement = Arrangement.SpaceBetween,
                   //Arrangement.Top, Arrangement.Bottom, Arrangement.Center, Arrangement.SpaceAround, Arrangement.SpaceBetween, Arrangement.SpaceEvenly
@@ -416,7 +425,7 @@ class MainActivity : ComponentActivity() {
                      // .fillMaxSize(0.5f)     // Specific height / width
                      // .height(200.dp)        // Specific height
                      // .width(200.dp)         // Specific width
-                      .background(Color.Green),// Background Color
+                      .background(Color.White),// Background Color
                   horizontalAlignment = Alignment.CenterHorizontally, // Horizontal Alignment
                   //Alignment.End, Alignment.Start, Alignment.CenterHorizontally
                   verticalArrangement = Arrangement.SpaceBetween // Vertical Arrangement
@@ -425,7 +434,7 @@ class MainActivity : ComponentActivity() {
               ) {
 
                   Text(text = "Hello")
-                  Text(text = "Android", color = Color.Red)
+                  Text(text = "Android", color = Color.Black)
                   Text(text = "Kotlin")
               }*/
 
@@ -496,14 +505,13 @@ fun ImageCard(
     }
 
 
-}*/
+}*//*
 
-/*
 @Composable
 fun ColorBox() {
 
     val color = remember {
-        mutableStateOf(Color.Green)
+        mutableStateOf(Color.White)
     }
 
     Box(
@@ -522,4 +530,80 @@ fun ColorBox() {
     )
 
 }*/
+@Composable
+fun ColumnScope.SurfaceItem(backgroundColor: Color, textColor: Color, text: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = backgroundColor),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text, color = textColor, style = TextStyle(
+                    color = Color.Black, fontSize = 50.sp
+                ), textAlign = TextAlign.Center
+            )
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackComposeExampleTheme {
+        ui()
+    }
+}
+
+@Composable
+fun ui() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(5.dp, Color.Red)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "A")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "B")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "C")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "D")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "E")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "F")
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "A")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "B")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "C")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "D")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "E")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "F")
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "A")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "B")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "C")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "D")
+            SurfaceItem(backgroundColor = Color.White, textColor = Color.Black, text = "E")
+            SurfaceItem(backgroundColor = Color.Black, textColor = Color.White, text = "F")
+        }
+    }
+}
