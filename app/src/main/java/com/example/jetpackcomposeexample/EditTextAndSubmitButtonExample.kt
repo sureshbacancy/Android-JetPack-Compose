e1dda0c756feb6f83eab2b80ed02b8a1cc354708
bacancy <suresh.prajapati@bacancy.com>
2024-06-27 15:06:39 +0530

    Date : 27-06-2024
    * Code updated
    - Code formatted
    - File separation
    - New class created to handle Ui

---
+++ app/src/main/java/com/example/jetpackcomposeexample/EditTextAndSubmitButtonExample.kt
@@ -0,0 +1,93 @@
+package com.example.jetpackcomposeexample
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+//Created by Suresh Prajapati - 13-02-2024
+
+@Composable
+fun ShowEditTextAndSubmitButtonView() {
+    val snackBarHostState = remember { SnackbarHostState() }
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
+
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(it)
+                .background(Color.White)
+                .verticalScroll(state = ScrollState(1))
+                .fillMaxSize(),
+        ) {
+
+            TextField(value = textFieldState,
+                onValueChange = {
+                    textFieldState = it
+                },
+                label = {
+                    Text(text = "Enter Your Name")
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                )
+
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+
+                if (textFieldState.isEmpty()) {
+                    scope.launch {
+                        snackBarHostState.showSnackbar("Please enter name")
+                    }
+                } else {
+                    scope.launch {
+                        snackBarHostState.showSnackbar(textFieldState)
+                    }
+                }
+
+
+            }) {
+                Text(text = "Submit")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetpackcomposeexample/SimpleStaticListViewWithText.kt
@@ -0,0 +1,55 @@
+package com.example.jetpackcomposeexample
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// Created by Suresh Prajapati - 14-02-2024
+
+@Composable
+fun ShowListViewUsingItems() {
+    LazyColumn {
+        items(100) {
+            Text(
+                text = "Item - ${it.plus(1)}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun ShowListViewUsingForLoopAndColumns() {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        for (i in 1..50) {
+            Text(
+                text = "item $i",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
