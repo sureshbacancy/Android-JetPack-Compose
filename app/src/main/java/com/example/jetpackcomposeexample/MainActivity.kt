81bc6b0e14b8541c539be221acf0b1157dd049ed
bacancy <suresh.prajapati@bacancy.com>
2024-07-01 17:23:59 +0530

    Date : 01-07-2024
    * Code updated
    - Text Customization added
    - Design Three Expandable View with text and arrow
    - Scrollable screen when all view visible

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,19 +3,34 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,10 +38,11 @@ import com.example.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ShowCustomTexts()
+            //ShowCustomTexts()
             //BlackAndWhiteCubeWithText()
             //ShowListViewUsingItems()
             //ShowListViewUsingForLoopAndColumns()
@@ -39,10 +55,13 @@ class MainActivity : ComponentActivity() {
             //TextWithBackGround()
             //ShowNormalHorizontalText()
             //ShowVerticalNormalText()
+            // TextSelection()
+            //ExpandableCardView()
         }
     }
 }
 
+@ExperimentalMaterial3Api
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
@@ -59,7 +78,9 @@ fun DefaultPreview() {
         //TextWithBackGround()
         //ShowNormalHorizontalText()
         //ShowVerticalNormalText()
-        //ShowBoxCustomization()
-        ShowCustomTexts()
+        //ShowBoxCustomization(
+        //ShowCustomTexts()
+        //TextSelection()
+        //ExpandableCardView()
     }
 }
