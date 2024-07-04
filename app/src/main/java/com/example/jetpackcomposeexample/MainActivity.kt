68f9b78877424fb77235d13e3da2ab1a3f189343
bacancy <suresh.prajapati@bacancy.com>
2024-07-04 16:00:23 +0530

    Date : 04-07-2024
    * Code updated
    - Custom Google Button Added
    - Text Field Added
    - Text Customization updated

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,37 +3,10 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,6 +30,8 @@ class MainActivity : ComponentActivity() {
             //ShowVerticalNormalText()
             // TextSelection()
             //ExpandableCardView()
+            //TextFieldExample(this)
+            CustomGoogleButton()
         }
     }
 }
@@ -65,6 +40,8 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
+    val context = LocalContext.current
+
     JetPackComposeExampleTheme {
         //BlackAndWhiteCubeWithText()
         //ShowListViewUsingItems()
@@ -82,5 +59,7 @@ fun DefaultPreview() {
         //ShowCustomTexts()
         //TextSelection()
         //ExpandableCardView()
+        //TextFieldExample(context)
+        CustomGoogleButton()
     }
 }
--- app/src/main/java/com/example/jetpackcomposeexample/TextCustomization.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/TextCustomization.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,13 +17,11 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ShowCustomTexts(){
+fun ShowCustomTexts() {
     Row(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -32,8 +29,7 @@ fun ShowCustomTexts(){
             modifier = Modifier
                 .fillMaxSize()
                 .weight(1f)
-                .background(Color.White)
-            ,
+                .background(Color.White),
             horizontalAlignment = Alignment.Start,
 
             ) {
@@ -86,8 +82,7 @@ fun ShowCustomTexts(){
             modifier = Modifier
                 .fillMaxSize()
                 .weight(1f)
-                .background(Color.White)
-            ,
+                .background(Color.White),
             horizontalAlignment = Alignment.Start,
 
             ) {
@@ -138,13 +133,17 @@ fun ShowCustomTexts(){
     }
 }
 
-val aa = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel velit eu dui luctus tincidunt. Ut efficitur justo et dui volutpat tincidunt. Proin quis ante id arcu pharetra gravida at a libero. Nulla facilisi."
-val bb = " Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti."
-val cc = " Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti. Quisque consequat aliquet erat, sit amet mollis risus eleifend id. Vivamus nec malesuada nisi. Sed fringilla diam lectus, id rutrum ex lobortis id. Maecenas varius, ligula sit amet congue tristique, enim nulla consectetur quam, ac feugiat erat orci a mi. Nunc convallis elit a ex congue, sit amet ullamcorper quam sagittis. Maecenas sit amet semper lectus. Vivamus sed eros in urna tempor lobortis."
+val aa =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel velit eu dui luctus tincidunt. Ut efficitur justo et dui volutpat tincidunt. Proin quis ante id arcu pharetra gravida at a libero. Nulla facilisi."
+val bb =
+    " Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti."
+val cc =
+    " Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti. Quisque consequat aliquet erat, sit amet mollis risus eleifend id. Vivamus nec malesuada nisi. Sed fringilla diam lectus, id rutrum ex lobortis id. Maecenas varius, ligula sit amet congue tristique, enim nulla consectetur quam, ac feugiat erat orci a mi. Nunc convallis elit a ex congue, sit amet ullamcorper quam sagittis. Maecenas sit amet semper lectus. Vivamus sed eros in urna tempor lobortis."
 
 @Composable
-fun TextSelection(){
-    val text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel velit eu dui luctus tincidunt. Ut efficitur justo et dui volutpat tincidunt. Proin quis ante id arcu pharetra gravida at a libero. Nulla facilisi. Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti. Quisque consequat aliquet erat, sit amet mollis risus eleifend id. Vivamus nec malesuada nisi. Sed fringilla diam lectus, id rutrum ex lobortis id. Maecenas varius, ligula sit amet congue tristique, enim nulla consectetur quam, ac feugiat erat orci a mi. Nunc convallis elit a ex congue, sit amet ullamcorper quam sagittis. Maecenas sit amet semper lectus. Vivamus sed eros in urna tempor lobortis."
+fun TextSelection() {
+    val text =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel velit eu dui luctus tincidunt. Ut efficitur justo et dui volutpat tincidunt. Proin quis ante id arcu pharetra gravida at a libero. Nulla facilisi. Sed posuere ligula id odio faucibus, eget gravida turpis congue. Integer id nulla nec justo rhoncus ultrices. Nulla facilisi. Suspendisse potenti. Quisque consequat aliquet erat, sit amet mollis risus eleifend id. Vivamus nec malesuada nisi. Sed fringilla diam lectus, id rutrum ex lobortis id. Maecenas varius, ligula sit amet congue tristique, enim nulla consectetur quam, ac feugiat erat orci a mi. Nunc convallis elit a ex congue, sit amet ullamcorper quam sagittis. Maecenas sit amet semper lectus. Vivamus sed eros in urna tempor lobortis."
 
     val annotatedString = with(AnnotatedString.Builder()) {
         append(text)
